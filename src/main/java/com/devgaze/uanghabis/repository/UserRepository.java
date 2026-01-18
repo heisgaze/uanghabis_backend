acd9db30ef5d5dca86efbef34ea06244f691269c
Galang Rizky <galangmazaya001@gmail.com>
2026-01-18 18:42:55 +0700

    feat: add User Repository

--- src/main/java/com/devgaze/uanghabis/repository/UserRepository.java
+++ src/main/java/com/devgaze/uanghabis/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.devgaze.uanghabis.repository;
 
-public interface UserRepository {
+import com.devgaze.uanghabis.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
 }
