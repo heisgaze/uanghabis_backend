e42d50d6be1ae88295f9ada7840b72194855cdef
Galang Rizky <galangmazaya001@gmail.com>
2026-01-23 10:26:09 +0700

    feat: add username & email lookup methods to user repository

--- src/main/java/com/devgaze/uanghabis/repository/UserRepository.java
+++ src/main/java/com/devgaze/uanghabis/repository/UserRepository.java
@@ -4,9 +4,14 @@ import com.devgaze.uanghabis.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
+    boolean existsByName(String name);
+    Optional<User> findByName(String name);
 }
