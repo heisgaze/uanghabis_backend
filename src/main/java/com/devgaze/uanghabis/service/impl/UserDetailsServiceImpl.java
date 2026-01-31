a19dead6b7238efd05b4169a5bb5d4a8aa143a72
Galang Rizky <galangmazaya001@gmail.com>
2026-01-31 19:22:06 +0700

    add user detail service impl

---
+++ src/main/java/com/devgaze/uanghabis/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.devgaze.uanghabis.service.impl;
+
+import com.devgaze.uanghabis.entity.User;
+import com.devgaze.uanghabis.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements org.springframework.security.core.userdetails.UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with Email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
