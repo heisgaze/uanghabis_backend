cf27cc56bd25ac5317bcf0221fa1b67865e4fa1c
Galang Rizky <galangmazaya001@gmail.com>
2026-01-31 19:27:32 +0700

    fix: type parameter of notification repository

--- src/main/java/com/devgaze/uanghabis/repository/NotificationRepository.java
+++ src/main/java/com/devgaze/uanghabis/repository/NotificationRepository.java
@@ -1,4 +1,9 @@
 package com.devgaze.uanghabis.repository;
 
-public interface NotificationRepository {
+import com.devgaze.uanghabis.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 }
